1aba983d03cc4c1e0791a01b3118bd069248bec1
djjproject <djj9404@gmail.com>
2020-03-07 22:47:12 +0900

    add samba_fix.sh

---
+++ smbfix/samba_fix.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Yellow Terminal message
+function output { 
+	YELLOW='\033[0;33m'
+	NC='\033[0m'
+	echo -e "\n${YELLOW}$1${NC}"
+}
+
+# make backup android samba service.
+output "Make backup for android samba service..."
+mv /system/bin/smbd /system/bin/smbd_bak
+mv /system/bin/nmbd /system/bin/nmbd_bak
+
+# install smbd nmbd.
+output "Update repository index and install samba server..."
+apt update
+apt install samba
+
+# make backup default smb.conf
+output "Make backup for default smb.conf file..."
+mv /etc/samba/smb.conf /etc/samba/smb.conf.bak
+
+# enable custom smb.conf
+output "Enable custom smb.conf configuration file..."
+wget https://github.com/djjproject/android_over_linux/raw/master/smbfix/smb.conf -O /etc/samba/smb.conf
+
+# reboot
+output "Installation Finished."
+output "You must reboot device."
+read -p "Do you want reboot? enter y. : " reboot
+if [ "$reboot" = "y" ]; then
+	sync
+	/sbin/reboot
+fi
+
+
+
