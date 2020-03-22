3777dc996f805af51825b5d22853e063abe5ae60
djjproject <djj9404@gmail.com>
2020-03-22 14:00:31 +0900

    fix mindlnad install script

--- minidlna_1.2.1/minidlna-install.sh
+++ minidlna_1.2.1/minidlna-install.sh
@@ -12,10 +12,11 @@ service minidlna stop
 
 output "remove old binary and configuration files..."
 rm /usr/local/sbin/minidlnad
+rm /usr/sbin/minidlnad
 rm /etc/minidlna.conf
 
 output "download minidlnad binary and minidlna.conf..."
-wget https://github.com/djjproject/android_over_linux/raw/master/minidlna_1.2.1/minidlnad -O /usr/local/sbin/minidlnad
+wget https://github.com/djjproject/android_over_linux/raw/master/minidlna_1.2.1/minidlnad -O /usr/sbin/minidlnad
 wget https://github.com/djjproject/android_over_linux/raw/master/minidlna_1.2.1/minidlna.conf -O /etc/minidlna.conf
 
 output "fix permissions..."
