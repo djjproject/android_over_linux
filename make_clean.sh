5f33eb233253b58050c4aa893498e4a972ec7f1b
djjproject <djj9404@gmail.com>
2020-06-20 18:10:54 +0900

    Update make_clean.sh

--- make_clean.sh
+++ make_clean.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
 echo "clean logs..."
+rm -rf /var/log/*.log
+rm -rf /var/log/messages
+rm -rf /var/log/apt/*
+rm -rf /var/log/mpd/*
+rm -rf /var/log/pure-ftpd/*
 
 echo "clean minidlna data..."
+service minidlna stop
+rm -rf /var/cache/minidlna/*
+
+echo "clean apt cache..."
+apt-get autoclean
+apt-get clean
 
 echo "clean home directory data..."
+rm -rf ~/.android
+rm -rf ~/.cache
+rm -rf ~/.config
+rm -rf ~/.local
+rm -rf ~/.nano
+rm -rf ~/.ssh
+rm -rf ~/.vim
+
+echo "clean tmp..."
+rm -rf /tmp/*
 
 echo "clean plexmediaserver data..."
+service plexmediaserver stop
+rm -rf /var/lib/plexmediaserver/*
 
 
